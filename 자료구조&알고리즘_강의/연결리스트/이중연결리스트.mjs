ca6c6a97ed96c3405ea73e26e1505125d312923a
spadecompany-sangsoo <sangsoo@spadecomapny.kr>
2023-06-23 17:53:05 +0900

    feat: 큐 강의 시청 중

---
+++ 자료구조&알고리즘_강의/연결리스트/이중연결리스트.mjs
@@ -0,0 +1,121 @@
+class Node {
+    constructor(data, next = null, prev = null) {
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+
+class DoublyLinkedList {
+    head = null;
+
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.count = 0;
+    }
+
+    //모든 데이터 출력
+    printAll() {
+        let currentNode = this.head;
+        let text = "[";
+
+        while(currentNode != null) {
+            console.log(currentNode);
+            text += currentNode.data;
+            currentNode = currentNode.next;
+
+            if(currentNode != null) {
+                text += ", ";
+            }
+        }
+        text += "]";
+        console.log(text);
+    }
+
+    //모든 데이터 제거
+    clear() {
+        this.head = null;
+        this.count = 0;
+    }
+
+    //인덱스 삽입
+    insertAt(index, data) {
+        if (index > this.count || index < 0) {
+            throw new Error('범위를 넘어갔습니다.');
+        }
+        let newNode = new Node(data);
+
+        if (index === 0) {
+            newNode.next = this.head;
+            if(this.head !== null) {
+                this.head.prev = newNode;
+            }
+            this.head = newNode;
+        } else if (index === this.count) {
+            newNode.next = null;
+            newNode.prev = this.tail;
+            this.tail.next = newNode;
+
+        } else {
+            let currentNode = this.head;
+
+            for (let i = 0; i < index - 1; i++) {
+                currentNode = currentNode.next;
+            }
+            newNode.next = currentNode.next;
+            currentNode.next = newNode;
+        }
+        this.count++;
+    }
+
+    //인덱스 마지막 삽입
+    insertLast(data) {
+        this.insertAt(this.count, data);
+    }
+
+    //인덱스 제거
+    deleteAt(index) {
+        if (index > this.count || index < 0) {
+            throw new Error('제거할 범위가 없습니다.');
+        }
+
+        let currentNode = this.head;
+
+        if(index === 0) {
+            let deleteNode = currentNode;
+            this.head = this.head.next;
+            this.count--;
+            return deleteNode;
+        } else {
+            for (let i = 0; i < index - 1; i++) {
+                currentNode = currentNode.next;
+            }
+            let deleteNode = currentNode.next;
+            currentNode.next = currentNode.next.next;
+            this.count--;
+            return deleteNode;
+        }
+    }
+
+    //마지막 인덱스 제거
+    deleteLast() {
+        return this.deleteAt(this.count - 1);
+    }
+
+    //특정 인덱스 불러오기
+    getNodeAt(index) {
+        if (index > this.count || index < 0) {
+            throw new Error('제거할 범위가 없습니다.');
+        }
+
+        let currentNode = this.head;
+        for (let i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+
+        return currentNode;
+    }
+}
+
+export { Node, DoublyLinkedList };
---
+++ 자료구조&알고리즘_강의/큐/큐.mjs
@@ -0,0 +1,14 @@
+/*
+enqueue - 데이터 삽입
+dequeue - 데이터 제거
+front - 맨 처음 들어간 데이터 참조
+isEmpty - 비었는 지 확인
+ */
+
+import { LinkedList } from '../연결리스트/연결리스트.mjs';
+
+class Queue {
+
+}
+
+export { Queue };
